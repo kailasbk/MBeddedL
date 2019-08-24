2fe062d1d785507d858a14a112b7bed30a1a7434
kailasbk <31834888+kailasbk@users.noreply.github.com>
2019-08-23 22:02:44 -0400

    Outline drive control

--- include/mbdl/drive.h
+++ include/mbdl/drive.h
@@ -4,25 +4,99 @@
 #include "main.h"
 
 namespace mbdl::drive {
+/**** Robot Position Variables ****/
+extern math::Vector position;
+extern double theta;
+extern math::Vector displacement;
+extern math::Vector heading;
+
+/**** Drivetrain Specification Variables ****/
 extern devices::Out* left;
 extern devices::Out* right;
 extern devices::In* encoders[3];
 extern double width;
-extern double theta;
 
-void tracking(void* params);
+/* 2x2 Transformation Matrix */
+extern math::Matrix transformation;
+
+/**** MODE TYPE MACROS ****/
+#define AUTO 0
+#define MANUAL 1
+extern uint8_t mode;
+
+/**** BUFFER TYPE MACROS ****/
+#define _TURN_ 0
+#define _ARC_ 1
+#define _LINE_ 2
+#define _TO_ 3
+extern char* buffer;
+
+/**
+ * Task for the tracking and drive control
+ * 
+ * @param params the extra pointer to the data used by the task
+ */
+void controlTask(void* params);
+
+/**
+ * Initializes the drivetrain in tank drive and 3 encoder odometry
+ * 
+ * @param w the width of the drivetrain
+ * @param l the pointer to the left side Motor or MotorGroup
+ * @param r the pointer to the right sides Motor or MotorGroup
+ * @param enc the array of pointers to the encoder objects
+ */
 void create(double w, devices::Out* l, devices::Out* r, devices::In* enc[3]);
 
+/**
+ * Sets the drivetrain using tank control
+ * 
+ * @param left the power within [-1, 1] for the left side
+ * @param right the power within [-1, 1] for the right side
+ */
 void tank(double left, double right);
+
+/**
+ * Sets the drivetrain using arcade control
+ * 
+ * @param pwr the forward power within [-1, 1]
+ * @param turn the turning power within [-1, 1]
+ */
 void arcade(double pwr, double turn);
 
+/**
+ * Turns the drivetrain arounds its center to the specified angle
+ * 
+ * @param goal the desired angle in radians
+ */
 void turn(double goal);
+
+/**
+ * Moves the robot in an arc
+ * 
+ * @param angle the angle of the arc in radians
+ * @param radius the radius of the arc in inches
+ */
+void arc(double angle, double radius);
+
+/**
+ * Move the robot in a straight line
+ * 
+ * @param distance in inches
+ */
+void line(double distance);
+
+/**
+ * Moves the drivetrain to the specified point
+ * 
+ * @param goal the desired coordinates in inches
+ */
 void to(math::Vector goal);
 
-extern math::Vector position;
-extern math::Vector displacement;
-extern math::Vector heading;
-extern math::Matrix transformation;
+/**
+ * States whether the robot has come to rest
+ */
+bool isSettled();
 }
 
 #endif
--- src/drive.cpp
+++ src/drive.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 
-// initialize all 2d vectors
 mbdl::math::Vector mbdl::drive::position(2);
 mbdl::math::Vector mbdl::drive::displacement(2);
 mbdl::math::Vector mbdl::drive::heading(2);
@@ -11,7 +10,6 @@ mbdl::devices::Out* mbdl::drive::right;
 mbdl::devices::In* mbdl::drive::encoders[3];
 double mbdl::drive::width;
 
-// initialize 2x2 transformation matrix
 mbdl::math::Matrix mbdl::drive::transformation(2, 2);
 
 void mbdl::drive::create(double w, devices::Out* l, devices::Out* r, devices::In* enc[3])
@@ -22,47 +20,58 @@ void mbdl::drive::create(double w, devices::Out* l, devices::Out* r, devices::In
         encoders[i] = enc[i];
     }
     width = w;
-    pros::Task tracking(mbdl::drive::tracking);
+    pros::Task tracking(mbdl::drive::controlTask);
 }
 
-void mbdl::drive::tracking(void* params)
+uint8_t mbdl::drive::mode = MANUAL;
+char* mbdl::drive::buffer = new char[64];
+
+void mbdl::drive::controlTask(void* params)
 {
     double dL = 0, dR = 0, dS = 0, L = 0, R = 0, S = 0, dTheta = 0;
     while (true) {
+        /**** TRACKING CODE ****/
+        {
+            // calculates distance traveled in last cycle
+            dL = encoders[0]->get() - L;
+            dR = encoders[1]->get() - R;
+            dS = encoders[2]->get() - S;
 
-        // calculates distance traveled in last cycle
-        dL = encoders[0]->get() - L;
-        dR = encoders[1]->get() - R;
-        dS = encoders[2]->get() - S;
+            // update absolute counts
+            L += dL;
+            R += dR;
+            S += dS;
 
-        // update absolute counts
-        L += dL;
-        R += dR;
-        S += dS;
+            // calculate anglular change
+            dTheta = ((R - L) / width) - theta;
 
-        // calculate anglular change
-        dTheta = ((R - L) / width) - theta;
+            // update absolute angle
+            theta += dTheta;
 
-        // update absolute angle
-        theta += dTheta;
+            // update heading vector
+            heading[0] = cos(theta - (dTheta / 2));
+            heading[1] = sin(theta - (dTheta / 2));
 
-        // update heading vector
-        heading[0] = cos(theta - (dTheta / 2));
-        heading[1] = sin(theta - (dTheta / 2));
+            // update transformation vector with heading vector
+            transformation[0][0] = heading[0];
+            transformation[1][0] = heading[1];
+            transformation[0][1] = -heading[1];
+            transformation[1][1] = heading[0];
 
-        // update transformation vector with heading vector
-        transformation[0][0] = heading[0];
-        transformation[1][0] = heading[1];
-        transformation[0][1] = -heading[1];
-        transformation[1][1] = heading[0];
+            // calculate displacement during last cycle
+            displacement[0] = (dR + dL) / 2;
+            displacement[1] = dS;
 
-        // calculate displacement during last cycle
-        displacement[0] = (dR + dL) / 2;
-        displacement[1] = dS;
-
-        // update position with translated displacement
-        position = position + (transformation * displacement);
-
+            // update position with translated displacement
+            position = position + (transformation * displacement);
+        }
+        /**** MOTION CODE ****/
+        {
+            if (mode == AUTO) {
+                // TODO: implement this code
+            }
+            // do nothing if in manual mode
+        }
         pros::delay(10);
     }
 }
@@ -81,8 +90,33 @@ void mbdl::drive::arcade(double pwr, double turn)
 
 void mbdl::drive::turn(double goal)
 {
+    buffer[0] = _TURN_;
+    buffer[1] = goal;
+}
+
+void mbdl::drive::arc(double angle, double radius)
+{
+    buffer[0] = _ARC_;
+    buffer[1] = angle;
+    buffer[1 + sizeof(double)] = radius;
+}
+
+void mbdl::drive::line(double distance)
+{
+    buffer[0] = _LINE_;
+    buffer[1] = distance;
 }
 
 void mbdl::drive::to(mbdl::math::Vector goal)
 {
+    buffer[0] = _TO_;
+    *(mbdl::math::Vector*)(buffer + 1) = goal;
+}
+
+bool mbdl::drive::isSettled()
+{
+    if (0) { // if some condition (not determined)
+        return true;
+    }
+    return false;
 }
